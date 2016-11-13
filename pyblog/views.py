68b2b75ec228e5b907704f4488390fb07c9f3e90
Administrator <Administrator@EDW2ZQTUHVBZW3E>
2016-11-13 20:21:18 +0800

    small fix

--- pyblog/views.py
+++ pyblog/views.py
@@ -5,3 +5,7 @@ def index(req):
     context['title'] = 'Index'
     context['message'] = 'Content from server'
     return render(req, 'index.html', context)
+
+def list_blogs(req):
+    context = {}
+    return render(req, 'list.html', context)
