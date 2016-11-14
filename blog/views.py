ed6bb45c7bc6e32e5bceb94e2a43ddd7fb4eb157
Michael Yan <mryanhaoxd@163.com>
2016-11-14 17:59:48 +0800

    finished nav

--- blog/views.py
+++ blog/views.py
@@ -6,6 +6,11 @@ def index(req):
     context['message'] = 'Content from server'
     return render(req, 'index.html', context)
 
+def signin(req):
+    context = {}
+    context['title'] = 'Sign In/Up'
+    return render(req, 'signin.html', context)
+
 def list_blogs(req):
     context = {}
     return render(req, 'list.html', context)
--- pyblog/urls.py
+++ pyblog/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from blog.views import index
+from blog.views import index, signin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url('',index),#default page
+    url('index/',index),#default page
+    url('signin/',signin),
 ]
